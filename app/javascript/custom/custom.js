30a2aac706461b4ef7cce292459b25e49d64a019
Elena-Weber <crochetmiracles@gmail.com>
2023-04-02 17:41:15 -0400

    fix js problem with turbo links

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -3,18 +3,26 @@ import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
 
-FilePond.registerPlugin(FilePondPluginImagePreview, FilePondPluginFileValidateType, FilePondPluginFileValidateSize);
+document.addEventListener("turbo:load", loadFilePond);
 
-FilePond.create(
-  document.querySelector("#post-images"),
-  {
-    credits: {},
-    storeAsFile: true,
-    allowMultiple: true,
-    allowReorder: true,
-    allowImagePreview: true,
-    allowFileTypeValidation: true,
-    acceptedFileTypes: ["image/*"],
-    allowFileSizeValidation: true,
-    maxFileSize: '5MB'
-});
+function loadFilePond() {
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginFileValidateType,
+    FilePondPluginFileValidateSize
+  );
+  
+  FilePond.create(
+    document.querySelector("#post-images"),
+    {
+      credits: {},
+      storeAsFile: true,
+      allowMultiple: true,
+      allowReorder: true,
+      allowImagePreview: true,
+      allowFileTypeValidation: true,
+      acceptedFileTypes: ["image/*"],
+      allowFileSizeValidation: true,
+      maxFileSize: '5MB'
+  });
+}
