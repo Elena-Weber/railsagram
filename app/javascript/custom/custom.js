80084793c04b4dd69badeb920081775861d1a944
Elena-Weber <crochetmiracles@gmail.com>
2023-04-02 17:04:31 -0400

    add filepond validations

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,15 +1,20 @@
 import * as FilePond from "filepond";
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
 
-FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.registerPlugin(FilePondPluginImagePreview, FilePondPluginFileValidateType, FilePondPluginFileValidateSize);
 
-// Get a reference to the file input element
-const inputElement = document.querySelector("#post-images");
-
-// Create a FilePond instance
-const pond = FilePond.create(inputElement, {
-  credits: {},
-  storeAsFile: true,
-  allowMultiple: true,
-  allowReorder: true
+FilePond.create(
+  document.querySelector("#post-images"),
+  {
+    credits: {},
+    storeAsFile: true,
+    allowMultiple: true,
+    allowReorder: true,
+    allowImagePreview: true,
+    allowFileTypeValidation: true,
+    acceptedFileTypes: ["image/*"],
+    allowFileSizeValidation: true,
+    maxFileSize: '5MB'
 });
