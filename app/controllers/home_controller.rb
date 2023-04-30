1c51163b179f1847d254b869444e61c44d56f6a2
Elena-Weber <crochetmiracles@gmail.com>
2023-04-30 16:22:52 -0400

    update suggestions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
+  before_action :set_suggestions
+
   def index
+  end
+
+  def set_suggestions
+    @suggestions = [current_user.followers]
+
+    [current_user.followers, current_user.followings].flatten.uniq.each do |person|
+      @suggestions.append([person.followers, person.followings])
+    end
+    
+    @suggestions = [@suggestions, User.all.sample(10)].flatten.uniq - [current_user.followings, current_user].flatten 
+    @suggestions = @suggestions.sample(5)
   end
 end
