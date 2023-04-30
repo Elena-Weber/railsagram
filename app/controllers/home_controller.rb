3d0e8348352bda115488d9b1908f444b4a0e7261
Elena-Weber <crochetmiracles@gmail.com>
2023-04-30 16:26:37 -0400

    update feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   before_action :set_suggestions
+  before_action :set_feed
 
   def index
   end
+
+  private
 
   def set_suggestions
     @suggestions = [current_user.followers]
@@ -10,8 +13,12 @@ class HomeController < ApplicationController
     [current_user.followers, current_user.followings].flatten.uniq.each do |person|
       @suggestions.append([person.followers, person.followings])
     end
-    
+
     @suggestions = [@suggestions, User.all.sample(10)].flatten.uniq - [current_user.followings, current_user].flatten 
     @suggestions = @suggestions.sample(5)
+  end
+
+  def set_feed
+    @feed = Post.where(user: [current_user, current_user.followings].flatten).order(created_at: :desc)
   end
 end
